1b35a66226c098ac45b7d97830299aab8ba448f6
up941374 <up941374@myport.ac.uk>
2022-09-13 20:53:00 +0200

    set up rdy

---
+++ script.js
@@ -0,0 +1,17 @@
+let map = document.querySelector("#map");
+
+let files = [
+""
+];
+
+function tostr(x){
+  let output = 'https://gpxstudio.github.io/?state=%7B%22urls%22:%5B'
+  for (let file of x) {
+    output = output + "%22https%3A%2F%2Fraw.githubusercontent.com%2FUP941374%2FGPS%2Fmain%2FGPX%2FSPACERY%2F" + file + "%22,"
+  }
+  mapsrc = output.slice(0, -1);
+  mapsrc = mapsrc + '%5D%7D&embed'
+  return mapsrc;
+}
+
+map.src = tostr(files);
